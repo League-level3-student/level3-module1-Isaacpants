c54ac88e53fb09bdbedd1a420652b887f260cb79
Isaacpants <dave@boyermoving.com>
2020-04-14 16:56:07 -0700

    Guest Book ip

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -2,62 +2,76 @@ package _00_Dynamic_Integer_Array;
 
 public class DynamicIntegerArray {
 	//1. Create a private int array. Don't initialize it.
-
+private int[] arr;
 	
 	public DynamicIntegerArray() {
 		//2. Initialize the int array to have 0 elements. 
 		//   This will prevent a null pointer exception
-	
+	arr = new int[0];
 	}
 	
 	//3. Complete the steps in the add method
 	public void add(int v) {
 		//A. create and initialize a new int array to be one 
 		//   element longer than the member array
-		
+		int[] arr1 = new int[arr.length+1];
 		//B. set the last element of your new array to 
 		//   the value passed into the method
-		
+		arr1[arr1.length-1]=v;
 		//C. iterate through the member array and 
 		//   copy every element from the member array 
 		//   to the new array
-		
+		for (int i = 0; i < arr.length; i++) {
+			arr1[i]=arr[i];
+		}
 		//D. set the member array equal to the new array.
+		arr=arr1;
 	}
 	
 	//4. Complete the steps in the get method
 	public int get(int location) {
-		//A. Return the value of the memeber array at the location passed in
-		return 0;
+		//A. Return the value of the member array at the location passed in
+		return arr[location];
 	}
 	
 	//5. Run the DynamicArrayTest to see if you are correct so far.
-	
+
 	
 	//6. Complete the steps in the set method
 	public void set(int v, int location) {
 		//A. set the variable at the location passed in to the method
 		//   to the new value v
+		arr[location]=v;
+		
 	}
 	
 	//7. Complete the steps in the insert method
 	public void insert(int v, int location) {
 		//A. create and initialize a new int array to be one 
 		//   element longer than the member array
-		
+		int[] arr2 = new int[arr.length+1];
 		//B. Make a for loop that iterates through the new array
+for (int i = 0; i < arr2.length; i++) {
+	
 
 			//C. if i is less than location:
 		    //		set the element at i of the new array to the element at i of the member array
-			
+			if(i<location) {
+				arr2[i]=arr[i];
+			}else if(i>location){
 			//D. else if i is greater than location:
 		    //		set the element at i of the new array to the i - 1 element of the member array
-			
+				arr2[i]=arr[i-1];
+			}else {
 			//E. else, set the element at i of the new array to the value v
-		
-		
+				arr2[i]=v;
+				
+}
+}
+			arr=arr2;
 		//F. set the member array equal to the new array
-	
+		
+
 	}
 	
 	//8. Run the tests again and check your progress
@@ -65,31 +79,39 @@ public class DynamicIntegerArray {
 	//9. Complete the steps in the remove method
 	public void remove(int location) {
 		//A. create a new array that is one element smaller than the member array
-		
+		int[] arr3 = new int[arr.length-1];
 		//B. make a for loop to iterate through the member array
-		
+		for (int i = 0; i < arr.length; i++) {
 			//C. if i  is less than location
 			//		set the element at i of the new array to the element at i of the member array
-			
+			if(i<location) {
+				arr3[i]=arr[i];
+			}else if(i>location) {
 			//D. else if i  is greater than location
 			//		set the element at i - 1 of the new array to the element at i of the member array
-			
+			arr3[i-1]=arr[i];
+			}else {
 			//E. else, continue;
-			
+			continue;
+			}
+		}
 		//F. set the member array equal to the new array
+		arr=arr3;
 	}
 	
 	//10. Run the tests again the see if you are correct so far
 	
 	//11. Complete the size method so that it returns the length of the member array.
 	public int size() {
-		return 0;
+		return arr.length;
 	}
 	
 	//12. Complete the clear array so that it sets the member array 
 	//    equal to a new integer array of size 0
 	public void clear() {
+		int[] arr4 = new int[0];
+		arr=arr4;
 	}
 	
-	//13. Run the test again to see if you are finished.
+	//13. Run the test again to see if you are finished. 
 }
--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,15 +1,60 @@
 package _01_IntroToArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener{
+	ArrayList<String> list = new ArrayList<String>();
+
+	JFrame jf = new JFrame();
+	JPanel jp = new JPanel();
+	JButton jb = new JButton();
+	JButton jb1 = new JButton();
+	public static void main(String[] args) {
+		
+		
+	}
+	void setup() {
+		jf.add(jp);
+		jp.add(jb);
+		jp.add(jb1);
+		jf.setVisible(true);
+		jb.addActionListener(this);
+		jb1.addActionListener(this);
+	
+		jb.setText("Add Name");
+		jb1.setText("View Names");
+	
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+	
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
+	
 	// Guest #1: Bob Banders
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
 	
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+	
+		if(e.getSource().equals(jb)) {
+		String name = JOptionPane.showInputDialog("What is the name");
+		list.add(name);
+		
+		}
+	}
+
 	
 	
 }
